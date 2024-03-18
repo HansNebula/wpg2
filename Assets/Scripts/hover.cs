54707491ede9770125516adf5a61ede0477f502b
HansNebula <hatfansahrul1@gmail.com>
2024-03-19 05:28:51 +0700

    progress 1

---
+++ Assets/Scripts/hover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hover : MonoBehaviour
+{
+   public GameObject opsi;
+
+   void Start(){
+    opsi.SetActive(false);
+   }
+
+   void OnMouseOver(){
+    opsi.SetActive(true);
+   }
+
+   void OnMouseExit(){
+    opsi.SetActive(false);
+   }
+}
