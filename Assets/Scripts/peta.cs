c8721f237cb665e7264149a7b56765e36368c65e
HansNebula <hatfansahrul@gmail.com>
2024-03-25 20:13:44 +0700

    mekanisme peta belum jadi

---
+++ Assets/Scripts/peta.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class peta : MonoBehaviour
+{
+   public GameObject mainMap;
+   public GameObject[] subMap;
+
+   public void showMain(){
+    mainMap.SetActive(true);
+   }
+
+   public void showSubMap(int n){
+    subMap[n].SetActive(true);
+   }
+
+   public void hideMain(){
+    mainMap.SetActive(false);
+   }
+
+   public void hideSubMap(int n){
+    subMap[n].SetActive(false);
+   }
+}
